d3bfe1d6a4d1aac9e6da732999de3fe6b8ba6652
lingfe <13068326391>
2017-11-27 18:09:31 +0800

    add update Signed-off-by: lingfe <13068326391>

---
+++ db/lemonDB.sql
@@ -0,0 +1,19 @@
+
+
+/*柠檬公益动态*/
+DROP TABLE IF EXISTS lemonDynamic;
+CREATE TABLE lemonDynamic(
+	id VARCHAR(32) NOT NULL COMMENT '柠檬公益动态表id标识',
+	personalId VARCHAR(32) NULL COMMENT '编写者',
+	title VARCHAR(255) NOT NULL COMMENT '标题',
+	imgUrl VARCHAR(255) NOT NULL COMMENT '图片地址',
+	content VARCHAR(255) NOT NULL COMMENT '内容',
+	
+  `cdate` DATETIME DEFAULT NULL COMMENT '创建时间',
+  `mdate` DATETIME DEFAULT NULL COMMENT '最后修改时间',
+  `creator` VARCHAR(32) DEFAULT NULL COMMENT '创建人',
+  `uman` VARCHAR(32) DEFAULT NULL COMMENT '修改人',
+  `df` INT(11) DEFAULT '0' COMMENT '是否删除',
+  `version` INT(11) DEFAULT '0' COMMENT '数据版本',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
