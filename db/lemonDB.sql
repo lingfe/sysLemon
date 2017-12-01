9783286bf228b09f7a3b2e12a7ff1b72df45be04
lingfe <13068326391>
2017-12-01 18:33:38 +0800

    update Signed-off-by: lingfe <13068326391>

--- db/lemonDB.sql
+++ db/lemonDB.sql
@@ -17,3 +17,23 @@ CREATE TABLE lemonDynamic(
   `version` INT(11) DEFAULT '0' COMMENT '数据版本',
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+/*积分交易*/
+DROP TABLE IF EXISTS integralTransaction;
+CREATE TABLE integralTransaction(
+	id VARCHAR(32) NOT NULL COMMENT '积分交易表id标识',
+	personalId VARCHAR(32) NOT NULL COMMENT '积分发送者',
+	lq_personalId VARCHAR(32) NULL  COMMENT '积分领取者',
+	integral FLOAT NOT NULL COMMENT '积分',
+	pwd VARCHAR(32) NOT NULL COMMENT '领取密匙',
+	state INT(11) DEFAULT 0 COMMENT '状态,0=未领取,1=已领取',
+	
+  `cdate` DATETIME DEFAULT NULL COMMENT '创建时间',
+  `mdate` DATETIME DEFAULT NULL COMMENT '最后修改时间',
+  `creator` VARCHAR(32) DEFAULT NULL COMMENT '创建人',
+  `uman` VARCHAR(32) DEFAULT NULL COMMENT '修改人',
+  `df` INT(11) DEFAULT '0' COMMENT '是否删除',
+  `version` INT(11) DEFAULT '0' COMMENT '数据版本',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
